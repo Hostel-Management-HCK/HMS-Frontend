cdb68f538eef32a6231e40c709c7d916ab0a2b47
AngelShahi <np03cs4a220304@heraldcollege.edu.np>
2024-04-09 22:40:32 +0545

    Added Admin Dashboard inside src

---
+++ src/Admin/script.js
@@ -0,0 +1,10 @@
+const  sideMenu = document.querySelector('aside');
+const menuBtn = document.querySelector('#menu_bar');
+const closeBtn = document.querySelector('#close_btn');
+
+menuBtn.addEventListener('click',()=>{
+       sideMenu.style.display = "block"
+})
+closeBtn.addEventListener('click',()=>{
+    sideMenu.style.display = "none"
+})
