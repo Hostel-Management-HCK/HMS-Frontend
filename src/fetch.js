4ceeee228d5c5cc432c35fc37017422d8a2f36e9
AngelShahi <np03cs4a220304@heraldcollege.edu.np>
2024-04-09 12:09:30 +0545

    Added connected login and backend via api

---
+++ src/fetch.js
@@ -0,0 +1,34 @@
+async function makeRequest(method, url, token, headers = {}, body = null) {
+    const defaultHeaders = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        // Add more default headers if needed
+    };
+
+    const requestOptions = {
+        method: method.toUpperCase(),
+        headers: {
+            ...defaultHeaders,
+            ...headers
+        },
+        body: body ? JSON.stringify(body) : null
+    };
+
+    if (token != "") {
+        requestOptions.headers['Authorization'] = `Bearer ${token}`;
+    }
+
+    try {
+        const response = await fetch(url, requestOptions);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        return {error:error,message:"Error fetching the data "};
+    }
+}
